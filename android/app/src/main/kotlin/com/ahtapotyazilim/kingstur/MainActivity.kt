60b612796f4c0880345aed95917fc631b989483b
Fatih KAYA <fatih96kaya@gmail.com>
2024-05-28 13:19:26 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/ahtapotyazilim/kingstur/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ahtapotyazilim.kingstur
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: WebView(
+            initialUrl: 'https://kingstur.com',
+            javascriptMode: JavascriptMode.unrestricted));
+  }
+}
